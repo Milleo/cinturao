f47d26c8e1d3f799da5dafb27b7d5661cacef4ef
Rafael Milleo Carrenho <rcarrenho@blockbit.com>
2018-05-07 14:29:59 -0300

    Configuracoes de LINT

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "parser": "babel-eslint",
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "ecmaVersion": 6,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "meteor"
+    ],
+    "extends": ["plugin:meteor/recommended"],
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "react/jsx-uses-vars": [2]
+    },
+    
+};
