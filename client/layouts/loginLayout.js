91533f754f55210082888042ff6498c8a1886d1f
Rafael Milleo Carrenho <rcarrenho@blockbit.com>
2018-05-11 18:23:04 -0300

    Removendo dependencias nao usadas

--- client/layouts/loginLayout.js
+++ client/layouts/loginLayout.js
@@ -2,7 +2,6 @@ import React, { Component } from "react"
 import { Grid, Message } from "semantic-ui-react"
 import { Link } from "react-router-dom"
 import LanguageSelector from "../components/languageSelector"
-import Login from "../modules/frontpage/login"
 
 class LoginLayout extends Component{
 
@@ -11,7 +10,6 @@ class LoginLayout extends Component{
       <LanguageSelector onChangeCallback={this.props.langChangeCallback} />
       <Grid verticalAlign="middle" centered style={{ height: "100%" }}>
         <Grid.Column width={5}>
-          <Login {...this.props} />
           { this.props.children }
         </Grid.Column>
       </Grid>
