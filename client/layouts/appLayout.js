a90c06854dada63abbe90a29c685e3dfd4768a3c
Rafael Milleo Carrenho <rcarrenho@blockbit.com>
2018-05-11 18:22:36 -0300

    Adicionando barra de level e informacoes do jogador

--- client/layouts/appLayout.js
+++ client/layouts/appLayout.js
@@ -1,30 +1,43 @@
 import React, { Component } from "react"
 import { withRouter } from "react-router-dom"
-import Meteor from "meteor/meteor"
-import { Container, Icon, Menu } from "semantic-ui-react"
+import { Container, Icon, Menu, Progress } from "semantic-ui-react"
 import LanguageSelector from "../components/languageSelector"
 import T from "../components/translation"
 import { Link } from "react-router-dom"
+import { withTracker } from "meteor/react-meteor-data"
 
 class AppLayout extends Component{
   constructor(props){
     super(props)
   }
-  
+
+ 
   render(){
+    if(!this.props.user)
+      return <div />
+
+    const { profile } = this.props.user
+
     return <Container>
       <Menu>
         <Menu.Item as={Link} to="/"><T>menu.main</T></Menu.Item>
         <Menu.Menu position="right">
-          <Menu.Item name="logout" onClick={this.props.handleLogout}><Icon name="sign out" /><T>menu.logout</T></Menu.Item>
           <LanguageSelector onChangeCallback={this.props.langChangeCallback} spaced={false} />
+          <Menu.Item name="logout" onClick={this.props.handleLogout}><Icon name="sign out" /><T>menu.logout</T></Menu.Item>
         </Menu.Menu>
       </Menu>
       <section id="main">
+        <Progress value={profile.xp} total={100} progress>
+          Level { profile.level } 
+        </Progress>
         {this.props.children}
       </section>
     </Container>
   }
 }
 
-export default AppLayout
+export default withTracker( (props) => {
+  const user = Meteor.user()
+
+  return { user }
+})(AppLayout)
