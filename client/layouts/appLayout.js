0ff623c789c36274221a98c0fb7588106047e393
Rafael Milleo Carrenho <rcarrenho@blockbit.com>
2018-08-30 17:02:50 -0300

    Alterando layout da aplicação

--- client/layouts/appLayout.js
+++ client/layouts/appLayout.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import { withRouter } from "react-router-dom"
-import { Container, Grid, Icon, Menu, Progress, Segment } from "semantic-ui-react"
+import { Container, Dropdown, Grid, Icon, Input, Menu, Progress, Segment } from "semantic-ui-react"
 import LanguageSelector from "../components/languageSelector"
 import PlayerCard from "../components/PlayerCard"
 import T from "../components/translation"
@@ -25,18 +25,22 @@ class AppLayout extends Component{
 
     const { profile } = this.props.user
 
-    return <Container>
-      <Menu>
+    return <div>
+      <Menu stackable fixed="top">
         <Menu.Item as={Link} to="/"><Icon name="home" /><T>menu.main</T></Menu.Item>
         { profile.status != "new" && <Menu.Item as={Link} to="/training"><Icon name="refresh" /><T>menu.training</T></Menu.Item> }
         { profile.status != "new" && <Menu.Item as={Link} to="/fight"><Icon name="trophy" /><T>menu.fight</T></Menu.Item> }
         { profile.status != "new" && <Menu.Item as={Link} to="/sponsors"><Icon name="tag" /><T>menu.sponsors</T></Menu.Item>}
+        { profile.status != "new" && <Menu.Item as={Link} to="/friends"><Icon name="users" /><T>menu.friends</T></Menu.Item>}
+        { profile.status != "new" && <Menu.Item as={Link} to="/ranking"><Icon name="signal" /><T>menu.ranking</T></Menu.Item>}
         <Menu.Menu position="right">
+          <Menu.Item><Input icon="search" placeholder="Buscar..." /></Menu.Item>
+          { profile.status != "new" && <Menu.Item as={Dropdown}><Icon name="globe" /></Menu.Item>}
           <LanguageSelector onChangeCallback={this.props.langChangeCallback} spaced={false} />
           <Menu.Item name="logout" onClick={this.props.handleLogout}><Icon name="sign out" /><T>menu.logout</T></Menu.Item>
         </Menu.Menu>
       </Menu>
-      <Grid>
+      <Grid padded>
         <Grid.Row columns={2}>
           <Grid.Column width={12}>
             <section id="main">
@@ -48,7 +52,7 @@ class AppLayout extends Component{
           </Grid.Column>
         </Grid.Row>
       </Grid>
-    </Container>
+    </div>
   }
 }
 
